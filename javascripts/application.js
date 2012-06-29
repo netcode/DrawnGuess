b127d3162162206578c4bcc72eb2ea639d63475d
Moustafa Badawy <moustafa.badawym@gmail.com>
2012-06-29 17:05:01 +0200

    Initial Commit

---
+++ javascripts/application.js
@@ -0,0 +1,103 @@
+
+!(function(global){
+
+	var Application = function(){
+		
+		if(!global["gapi"]) {
+			throw "Unable to load application, gapi not available";
+		}
+
+		gapi.hangout.onApiReady.add(this.onApiReady.bind(this));
+
+		this.currentPlayerIndex = 0;
+		this.players = [];
+
+	};
+
+	/**
+	 * @onApiReady - Fired by gapi when RPC layer is ready.
+	*/
+	Application.prototype.onApiReady = function(event) {
+		if(event.isApiReady == true)
+		{
+			/*
+			 * Ready to start our application.
+			*/
+			console.log("Application is ready to run!");
+
+			//this.players = $.map(gapi.getParticipants())
+
+			/*
+			 * Monitor for client changes
+			*/
+			gapi.hangout.onParticipantsChanged.add(this.refreshParticipantsList.bind(this));
+
+			/*
+			 * Initially draw the participants
+			*/
+			this.displayParticipants();
+		}
+	};
+
+	/*
+	 * refreshParticipantsList
+	*/
+	Application.prototype.refreshParticipantsList = function(event)
+	{
+		/*
+		 * recall the draw method
+		*/
+		this.displayParticipants();
+	}
+
+	/*
+	 * Display participants
+	*/
+	Application.prototype.displayParticipants = function()
+	{
+		/*
+		 * Remove the UL Container
+		*/
+		$("#container").html('');
+
+		/*
+		 * Get enabled participants
+		*/
+		var clients = gapi.hangout.getParticipants();
+
+		/*
+		 * Ul Container
+		*/
+		var container = document.createElement("ul");
+
+		/*
+		 * Loop the participants
+		*/
+		for(var i = 0; i < clients.length; i++)
+		{
+			var currentClient = clients[i];
+			var li = document.createElement("li");
+
+			if(currentClient.hasAppEnabled == true)
+			{
+				var image = "<img src=\"" + currentClient.person.image.url + "\"  width=\"32\" height=\"32\" />";
+				li.innerHTML = image + " " + currentClient.person.displayName;
+			}
+			else
+			{
+				li.innerHTML = "Unknown Participant";
+				li.setAttribute("class", "unknown");
+			}
+
+			container.appendChild(li);
+		}
+
+		$('#container').append(container);
+		//document.getElementById("container").appendChild(container);
+	}
+
+	/*
+	 * Return the instance
+	*/
+	return new Application();
+})(window || {})
